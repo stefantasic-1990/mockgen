9d2db88dd66e16974edd34ac75bcccc0da288c4e
Stefan Tasic <stefantasic@outlook.com>
2024-08-30 19:33:55 -0400

    add schema fetch connector

--- mockgen.py
+++ mockgen.py
@@ -2,6 +2,9 @@ import requests
 import configparser
 import os
 from requests.exceptions import HTTPError, RequestException
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from snowflake import connector
 
 ## read-in config
 config = configparser.ConfigParser()
@@ -15,9 +18,39 @@ api_key = config.get("OPENAI", "API_KEY")
 endpoint = config.get("OPENAI", "ENDPOINT")
 model = config.get("OPENAI", "MODEL")
 tmp_dir = config.get("MOCKGEN", "TMP_DIR")
+snowflake_private_key_path = config.get("SNOWFLAKE", "SNOWFLAKE_PRIVATE_KEY_PATH")
 
 prompt = [{"role": "user", "content": "Give me three rows by three columns of mock CSV data, and do not give me any other text. Three tables. Table names are A, B, C, include separately inside CSV before the data."}]
 
+def encode_private_key(private_key_content):
+
+    private_key_en = private_key_content.encode()
+    private_key_sr = serialization.load_pem_private_key(
+        private_key_en, password=None, backend=default_backend()
+    )
+    private_key_bytes = private_key_sr.private_bytes(
+        encoding=serialization.Encoding.DER,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption(),
+    )
+    return private_key_bytes
+
+def get_schemas(input):
+
+    with open(f"PRIVATE_KEY_PATH", "r") as f:
+        private_key_contents = f.read()
+
+    conn = connector.connect(
+        user="devaccountairflow",
+        account="bj69762.ca-central-1.aws",
+        warehouse="ENGINEERS",
+        database="DEVELOPMENT",
+        schema="POINTS_PARTNER_FILE",
+        private_key=encode_private_key(private_key_contents),
+    )
+
+    cursor = conn.cursor()
+    
 def download_data(prompt):
 
     filename = f"{tmp_dir}/data.csv"
@@ -48,8 +81,6 @@ def download_data(prompt):
         file.write(response_text.strip("`").strip())
 
 download_data(prompt)
-
-def get_schemas(input):
 
 
 # def load_data():
